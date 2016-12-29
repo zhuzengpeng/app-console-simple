9fd4f786b7553260a9cc3852cc302a508e1aa3e9
zzp <zzp19860905@126.com>
2016-12-29 16:08:34 +0800

    取消没必要的NULL判断

--- src/main/java/com/thinkgem/jeesite/common/utils/SpringContextHolder.java
+++ src/main/java/com/thinkgem/jeesite/common/utils/SpringContextHolder.java
@@ -3,20 +3,11 @@
  */
 package com.thinkgem.jeesite.common.utils;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Date;
-
-import org.apache.commons.lang3.Validate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-
-import com.thinkgem.jeesite.common.config.Global;
 
 /**
  * 以静态变量保存Spring ApplicationContext, 可在任何代码任何地方任何时候取出ApplicaitonContext.
@@ -30,13 +21,10 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
 
 	private static ApplicationContext applicationContext = null;
 
-	private static Logger logger = LoggerFactory.getLogger(SpringContextHolder.class);
-
 	/**
 	 * 取得存储在静态变量中的ApplicationContext.
 	 */
 	public static ApplicationContext getApplicationContext() {
-		assertContextInjected();
 		return applicationContext;
 	}
 
@@ -45,7 +33,6 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T getBean(String name) {
-		assertContextInjected();
 		return (T) applicationContext.getBean(name);
 	}
 
@@ -53,7 +40,6 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
 	 * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
 	 */
 	public static <T> T getBean(Class<T> requiredType) {
-		assertContextInjected();
 		return applicationContext.getBean(requiredType);
 	}
 
@@ -61,9 +47,6 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
 	 * 清除SpringContextHolder中的ApplicationContext为Null.
 	 */
 	public static void clearHolder() {
-		if (logger.isDebugEnabled()){
-			logger.debug("清除SpringContextHolder中的ApplicationContext:" + applicationContext);
-		}
 		applicationContext = null;
 	}
 
@@ -83,10 +66,4 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
 		SpringContextHolder.clearHolder();
 	}
 
-	/**
-	 * 检查ApplicationContext不为空.
-	 */
-	private static void assertContextInjected() {
-		Validate.validState(applicationContext != null, "applicaitonContext属性未注入, 请在applicationContext.xml中定义SpringContextHolder.");
-	}
 }
